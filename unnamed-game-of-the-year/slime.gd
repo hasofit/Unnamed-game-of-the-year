1b810e728944e68b82080456c7764bdfa6fce3b4
hasofit <notsus0205@gmail.com>
2025-12-07 16:51:56 +0200

    Update slime.gd

--- unnamed-game-of-the-year/slime.gd
+++ unnamed-game-of-the-year/slime.gd
@@ -4,7 +4,7 @@ extends CharacterBody2D
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 @onready var player: CharacterBody2D = %Player
 @export var speed = 500
-@export var damage = 1
+@export var damage = 5
 func _physics_process(delta: float) -> void:
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction * speed
@@ -14,5 +14,5 @@ func _physics_process(delta: float) -> void:
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
-		body.take_damage(damage)
-		print("DAMGE")
+		body.enemy_damaging_count += 1
+		body.damage += damage
