37d13eb98b3ed3373066daf267124d0aa402e49a
mazlegsakin <123097352+dovrv@users.noreply.github.com>
2025-12-07 16:29:30 +0200

    slime

---
+++ unnamed-game-of-the-year/slime.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var player: CharacterBody2D = %Player
+@export var speed = 500
+@export var damage = 1
+func _physics_process(delta: float) -> void:
+	var direction = global_position.direction_to(player.global_position)
+	velocity = direction * speed
+	move_and_slide()
+	
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		body.health -= damage
