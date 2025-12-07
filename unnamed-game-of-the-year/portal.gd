459330c890a2e753db75869616ab78e52bf28356
hasofit <notsus0205@gmail.com>
2025-12-07 18:02:23 +0200

    fixed portal

--- unnamed-game-of-the-year/portal.gd
+++ unnamed-game-of-the-year/portal.gd
@@ -1,6 +1,9 @@
 extends Area2D
 
-@export var WorldToGo : String
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.is_in_group("Player"):
-		get_tree().change_scene_to_file(WorldToGo)
+@export_flags("World 1","Testing world") var WorldToGo
+
+func _on_body_entered(body: Node2D) -> void:
+	if WorldToGo == 1:
+		get_tree().change_scene_to_file("res://scenes/world_1.tscn")
+	if WorldToGo == 2:
+		get_tree().change_scene_to_file("res://scenes/testing_world.tscn")
