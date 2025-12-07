9e41fdd9009504d2d4a88394af1c8cfd26c7cf76
hasofit <notsus0205@gmail.com>
2025-12-07 14:39:16 +0200

    Basic player and menu

---
+++ unnamed-game-of-the-year/scripts/player.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+var dead = false
+var SPEEDX = 600.0
+var SPEEDY = 600
+
+
+func _physics_process(delta: float) -> void:
+	if !dead:
+		if not is_on_floor():
+			velocity += get_gravity() * delta
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var directionX := Input.get_axis("move_left", "move_right")
+	var directionY := Input.get_axis("move_up", "move_down")
+	if directionX:
+		velocity.x = directionX * SPEEDX
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEEDX)
+	if directionY:
+		velocity.y = directionY * SPEEDY
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEEDY)
+
+	move_and_slide()
